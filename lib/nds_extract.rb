8e4d49c3a5f72eece27070a7eb913ab1343993de
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-22 16:43:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,8 +6,33 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
+  pp nds[0]
+  
+  grand_total = 0
+  row_index = 0
+  while row_index < nds.length do
+    column_index = 0
+    #pp nds[row_index][0]
+    while column_index < nds[row_index].length do
+      inner_leng =  nds[row_index][column_index].length
+      inner_index = 0
+     # pp nds[row_index][column_index]
+      while inner_index < inner_leng do
+        # Explanation!
+        # vm[row][column][spinner]
+        # spinner is full of Hashes with keys :price and :name
+      # grand_total += vm[row_index][column_index][inner_index][:price]
+       # pp nds[row_index][column_index][inner_index]
+        inner_index += 1
+      end
+      column_index += 1
+    end
+    row_index += 1
+  end
+#p grand_total #=> 1192
+
   result = {}
-  nil
+  result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
